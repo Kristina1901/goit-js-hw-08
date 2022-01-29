9fea923b983996e94dbfe1c30adc06ad547d3b10
Kristina <ktuzova1901@gmail.com>
2022-01-29 17:16:48 +0200

    1th task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,38 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+// Описан в документации
+import SimpleLightbox from 'simplelightbox';
+// Дополнительный импорт стилей
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 
 console.log(galleryItems);
+function createGalleryCollection(arr) {
+  
+    return arr.map(({ preview, original, description }) => {
+        return `
+            <a class="gallery__link" href="${original}">
+           <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/>
+            </a>
+        `})
+        .join('')
+    
+};
+const container = document.querySelector('.gallery')
+const img = createGalleryCollection(galleryItems);
+container.insertAdjacentHTML('beforeend', img)
+container.addEventListener('click', nameImages)
+
+function nameImages(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains("gallery__image")) {
+    
+  }
+}
+const lightbox = new SimpleLightbox(".gallery a", {
+  scrollZoom: true,
+  captionDelay: 250,
+  captionsData: "alt",
+  doubleTapZoom: 2,
+});
